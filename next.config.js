3d55fcc7e5cb336a54f376b164d5a02b56ead264
dallind <61264144+dallind@users.noreply.github.com>
2023-01-10 19:03:05 -0700

    ""

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-};
+  reactStrictMode: true,
+}
 
-export default nextConfig
+module.exports = nextConfig
