f97b8cd804d3c4369f1574e8b869ce10cac1becc
dallind <61264144+dallind@users.noreply.github.com>
2023-01-16 17:29:13 -0700

    fix images

--- components/small/phones.js
+++ components/small/phones.js
@@ -6,19 +6,19 @@ const phones = [
         name: 'Apple iPhone 14 Plus',
         price_month: 85,
         total: 1019,
-        img: `${link}iPhone-14-Plus-red.png`
+        img: required(`${link}iPhone-14-Plus-red.png`)
     },
     {
         name: 'Google Pixel 7 Pro',
         price_month: 50,
         total: 589,
-        img: `${link}pixel7.png`
+        img: required(`${link}pixel7.png`)
     },
     {
         name: 'OnePlus Nord N20',
         price_month: 33,
         total: 389,
-        img: `${link}oneplus-nord-n20.png`
+        img: required(`${link}oneplus-nord-n20.png`)
     }
 ]
 
